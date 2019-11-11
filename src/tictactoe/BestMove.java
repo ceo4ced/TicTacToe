9c921947fd180a759597be17ece8fa00490d5794
Janelle <jayazeng@users.noreply.github.com>
2019-11-10 19:17:30 -0500

    added counting methods to count number of x or o in a winning path

--- src/tictactoe/BestMove.java
+++ src/tictactoe/BestMove.java
@@ -46,20 +46,26 @@ function minimax(board, depth, isMaximizingPlayer):
 public class BestMove {
 	private Gameboard g;
 	private ProbabilityTable table;
+	private Cell player;
 
-	public BestMove() {
-		g = new Gameboard();
-		table = new ProbabilityTable();
+	public BestMove(Gameboard game, ProbabilityTable t) {
+		g = game;
+		table = t;
+		if (game.turn % 2 == 0) {
+			player = Cell.X;
+		} else {
+			player = Cell.O;
+		}
 	}
 
-	public String findBestMove(Gameboard board) {
+	public String findBestMove() {
 		String bestMove = null;
 		int bestVal = 0;
 		for (int dim = 0; dim < 4; dim++) {
 			for (int row = 0; row < 4; row++) {
 				for (int col = 0; col < 4; col++) {
-					if (board.getCell(dim, row, col) == Cell.E) {
-						Gameboard fake = board;
+					if (g.getCell(dim, row, col) == Cell.E) {
+						Gameboard fake = g;
 						fake.setCell(dim, row, col);
 						int placeholder = minimax(fake, 4, true);
 						if (placeholder > bestVal) {
@@ -70,7 +76,7 @@ public class BestMove {
 			}
 		}
 		// if current move is better than bestMove
-		//   bestMove = current move
+		// bestMove = current move
 		return bestMove;
 	}
 
@@ -81,23 +87,23 @@ public class BestMove {
 			} else {
 				return -1;
 			}
-			//return valOfBoard 
+			// return valOfBoard
 		}
-		
+
 		if (depth == 4) {
 			return 0;
 		}
 		if (isMaximizingPlayer) {
-			int bestVal = Integer.MIN_VALUE; 
-			//for each move in board :
+			int bestVal = Integer.MIN_VALUE;
+			// for each move in board :
 			for (int dim = 0; dim < 4; dim++) {
 				for (int row = 0; row < 4; row++) {
 					for (int col = 0; col < 4; col++) {
 						if (board.getCell(dim, row, col) == Cell.E) {
 							Gameboard fake = board;
 							fake.setCell(dim, row, col);
-							int value = minimax(fake, depth+1, false);
-							bestVal = max(bestVal, value); 
+							int value = minimax(fake, depth + 1, false);
+							bestVal = max(bestVal, value);
 							return bestVal;
 						}
 					}
@@ -106,15 +112,15 @@ public class BestMove {
 
 		} else {
 			int bestVal = Integer.MAX_VALUE;
-			//for each move in board :
+			// for each move in board :
 			for (int dim = 0; dim < 4; dim++) {
 				for (int row = 0; row < 4; row++) {
 					for (int col = 0; col < 4; col++) {
 						if (board.getCell(dim, row, col) == Cell.E) {
 							Gameboard fake = board;
 							fake.setCell(dim, row, col);
-							int value = minimax(fake, depth+1, true);
-							bestVal = min(bestVal, value); 
+							int value = minimax(fake, depth + 1, true);
+							bestVal = min(bestVal, value);
 							return bestVal;
 						}
 					}
@@ -122,6 +128,141 @@ public class BestMove {
 			}
 		}
 		return 0;
+	}
+
+	private boolean findThree() {
+
+		return true;
+	}
+
+	public int count2DRow(int dim, int row) {
+		int count = 0;
+		for (int col = 0; col < 4; col++) {
+			if (g.getCell(dim, row, col) == player) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public int count2DCol(int dim, int col) {
+		int count = 0;
+		for (int row = 0; col < 4; row++) { // for col
+			if (g.getCell(dim, row, col) == player) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public int count2DDim(int row, int col) {
+		int count = 0;
+		for (int dim = 0; dim < 4; dim++) {// for col
+			if (g.getCell(dim, row, col) == player) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public int count2DDiagnolA(int dim) {
+		int count = 0;
+		for (int row = 0; row < 4; row++) { // for dim
+			if (g.getCell(dim, row, row) == player) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public int count2DDiagnolB(int dim) {
+		int count = 0;
+		for (int row = 0; row < 4; row++) { // for dim
+			if (g.getCell(dim, row, 3 - row) == player) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public int count3DRowDiagnolA(int row) {
+		int count = 0;
+		for (int dim = 0; dim < 4; dim++) { // for dim
+			if (g.getCell(dim, row, dim) == player) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public int count3DRowDiagnolB(int row) {
+		int count = 0;
+		for (int dim = 0; dim < 4; dim++) { // for dim
+			if (g.getCell(dim, row, 3 - dim) == player) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public int count3DColDiagnolA(int col) {
+		int count = 0;
+		for (int row = 0; row < 4; row++) { // for dim
+			if (g.getCell(row, row, col) == player) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public int count3DColDiagnolB(int col) {
+		int count = 0;
+		for (int row = 0; row < 4; row++) { // for dim
+			if (g.getCell(row, 3 - row, col) == player) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public int count3DDiagnolA() {
+		int count = 0;
+		for (int row = 0; row < 4; row++) { // for dim
+			if (g.getCell(row, row, row) == player) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public int count3DDiagnolB() {
+		int count = 0;
+		for (int row = 0; row < 4; row++) { // for dim
+			if (g.getCell(row, row, 3 - row) == player) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public int count3DDiagnolC() {
+		int count = 0;
+		for (int row = 0; row < 4; row++) { // for dim
+			if (g.getCell(row, 3 - row, 3 - row) == player) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public int count3DDiagnolD() {
+		int count = 0;
+		for (int row = 0; row < 4; row++) { // for dim
+			if (g.getCell(row, 3 - row, row) == player) {
+				count++;
+			}
+		}
+		return count;
 	}
 
 	private static int min(int a, int b) {
